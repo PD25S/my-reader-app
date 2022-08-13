67ece5a1a8f18c2decefd19479ca95b4970ef0b1
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-08-13 04:05:51 -0300

    feat: home page component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { HeaderEffect } from './styles'
+
+function Header() {
+    return (
+            <HeaderEffect>
+                <h1>MyReader Project</h1>
+                <p>Developed by Guilherme Minozzi e Marcelo Falchi / <a href="https://github.com/PD25S"> Project Page</a></p>
+            </HeaderEffect>
+    )
+}
+
+export default Header 
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HeaderEffect = styled.div`
+    height: 100px;
+    color: white;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+`
