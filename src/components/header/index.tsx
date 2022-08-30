985a282a1cb6a8d191f1172e8c3ca60bf9a3bd86
Guilherme Minozzi <minozzi60@gmail.com>
2022-08-30 19:49:16 -0300

    feat(components): add subtitle prop

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,12 +4,14 @@ import * as S from './styles'
 
 type HeaderProps = {
   title: string
+  subtitle?: string
 }
 
-export const Header: React.FC<HeaderProps> = ({ title }) => {
+export const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
   return (
     <S.Container>
       <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
     </S.Container>
   )
 }
