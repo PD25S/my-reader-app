e5c8fa13f4bd89530b570eac52ebd78932226e96
Guilherme Minozzi <minozzi60@gmail.com>
2022-09-07 13:04:24 -0300

    chore: add fallbacks polyfil

--- webpack/renderer.webpack.js
+++ webpack/renderer.webpack.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   resolve: {
@@ -6,7 +7,21 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, '../src')
     },
+    fallback: {
+      path: require.resolve('path-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      util: require.resolve('util/'),
+      assert: require.resolve('assert/'),
+      buffer: require.resolve('buffer'),
+      stream: require.resolve('stream-browserify'),
+    }
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    })
+  ],
+  target: 'electron-renderer',
   module: {
     rules: require('./rules.webpack'),
   },
